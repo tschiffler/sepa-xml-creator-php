8d475f847032e8757ececdaebd187808516fe250
Thomas Schiffler <tsc@ThomasSchiffler.de>
2013-12-12 14:00:32 +0100

    Testcase um Validierung erweitert

--- ueberweisungExample.php
+++ ueberweisungExample.php
@@ -23,7 +23,7 @@
 	 * Offset nicht übergeben
 	 * Ausführung -> Heute bzw. nächst möglich
 	 * 
-	 * Beispiel 1
+	 * Beispiel 2
 	 * heute = 1. Juni 2013
 	 * Offset 3
 	 * Ausführung -> 4. Juni 2013
@@ -56,6 +56,9 @@
 	$creator->addBuchung($buchung);
 	
 	// Nun kann die XML-Datei über den Aufruf der entsprechenden Methode generiert werden
-	echo $creator->generateSammelueberweisungXml();
-	
+	$sepaxml = $creator->generateSammelueberweisungXml();
+	echo $sepaxml;
+	file_put_contents('sepaueberweisung-example.xml', $sepaxml);
+	$creator->validateUeberweisungXml('sepaueberweisung-example.xml');
+	$creator->printXmlErrors();
 ?>
